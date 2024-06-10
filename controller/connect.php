7d6e23c7d150b8a1b989633b772dd399f8c8ed38
Khang Huai <khanghuai@infoview.com.my>
2024-06-10 12:14:13 +0800

    update

--- controller/connect.php
+++ controller/connect.php
@@ -1,24 +1,36 @@
 <?php
-$servername = "evacuation.mysql.database.azure.com";
-$username = "khanghuai";
-$password = "7z5x4c9v0B";
-$dbname = "evacuation_fyp";
+// $servername = "evacuation.mysql.database.azure.com";
+// $username = "khanghuai";
+// $password = "7z5x4c9v0B";
+// $dbname = "evacuation_fyp";
 
-// Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname );
+// // Create connection
+// // $conn = new mysqli($servername, $username, $password, $dbname );
 
+// $conn = mysqli_init();
+// mysqli_real_connect($conn, "evacuation.mysql.database.azure.com", "khanghuai", "7z5x4c9v0B", "evacuation_fyp", 3306);
+
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+
+$host = 'evacuation.mysql.database.azure.com';
+$username = 'khanghuai';
+$password = '7z5x4c9v0B';
+$db_name = 'evacuation_fyp';
+
+//Initializes MySQLi
 $conn = mysqli_init();
-mysqli_real_connect($conn, "evacuation.mysql.database.azure.com", "khanghuai", "7z5x4c9v0B", "evacuation_fyp", 3306);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+// Establish the connection
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+  die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-echo "
-    <script>
-        console.log('Database Connected successfully');
-    </script>
-";
 
 
 function getUsername($conn, $Uid){
