8671dd82405288887c06998d2268b70436857f87
Khang Huai <khanghuai@infoview.com.my>
2024-06-10 13:01:57 +0800

    update

--- controller/connect.php
+++ controller/connect.php
@@ -10,8 +10,7 @@
 //     die("Connection failed: " . $conn->connect_error);
 //   }
 
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+$conn = mysqli_init();
 mysqli_real_connect($conn, "evacuation.mysql.database.azure.com", "khanghuai", "7z5x4c9v0B", "evacuation_fyp", 3306, MYSQLI_CLIENT_SSL);
 
 
